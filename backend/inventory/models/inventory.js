3b189e28591221f60baa86852fefc8508a9a7939
MoBazmul <mohaarash001@gmail.com>
2024-10-09 21:29:51 +0300

    Adding the pharmacyid and id to the pharmacy model

--- backend/inventory/models/inventory.js
+++ backend/inventory/models/inventory.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
 
 const inventorySchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: uuid()
+  },
+  pharmacyId: {
+    type: String,
+    required: true
+  },
   medcineId: {
     type: String
   },
