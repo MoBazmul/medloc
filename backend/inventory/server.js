6933cb98e987bfff667c0aa2339688aecbaff570
MoBazmul <mohaarash001@gmail.com>
2024-10-11 20:49:05 +0300

    Adding the mainCOnfig file from shared library

--- backend/inventory/server.js
+++ backend/inventory/server.js
@@ -1,36 +1,14 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const corsOptions = require('../shared_library/config/corsOptions')
-const credentials = require('../shared_library/middleware/credentials')
-const { logger } = require('../shared_library/middleware/logEvents')
-const errorHandler = require('../shared_library/middleware/errorHandler')
-const notFoundErrorHandler = require('../shared_library/config/404')
-const connectDB = require('../shared_library/config/dbConn')
-const verifyJWT = require('../shared_library/middleware/verifyJWT.JS')
+const { errorHandler, notFoundErrorHandler, mainConfig, verifyJWT, PORT } = require('shared_library')
 
-const PORT = require('../shared_library/config/port')
-
-// Log message for each request
-app.use(logger)
-
-// Connect the DB
-connectDB()
-
-// CORS middleware
-app.use(credentials)
-app.use(cors(corsOptions))
-
-// Middleware for urlencoded data (forms data)
-app.use(express.urlencoded({ extended: false }))
-
-// Middleware for json data
-app.use(express.json())
+// Configure middleware
+mainConfig()
 
 // Routes
 app.use(verifyJWT)
 app.use('/medicines', require('./routes/api/v1/medicines'))
-app.use('/pharmacy', require('./routes/api/v1/pharmacy'))
+app.use('/pharmacies', require('./routes/api/v1/pharmacies'))
 
 // 404 error handler
 app.all('*', notFoundErrorHandler)
